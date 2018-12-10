fa4c8fda0401e431de35f547af6b5dcee0b1914c
Strike50 <lhoulgattebustamante@gmail.com>
2018-12-10 11:40:28 +0100

    Added abstract class Auditable and extending it in Link & Comment

--- src/main/java/com/gfi/springit/SpringitApplication.java
+++ src/main/java/com/gfi/springit/SpringitApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
+@EnableJpaAuditing
 public class SpringitApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/gfi/springit/domain/Comment.java
+++ src/main/java/com/gfi/springit/domain/Comment.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 @Entity
 @Data
 @NoArgsConstructor
-public class Comment {
+public class Comment extends Auditable{
 
     @Id
     @GeneratedValue
