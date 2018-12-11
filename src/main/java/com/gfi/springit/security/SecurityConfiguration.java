e625458875f94cde3965422149db34c025954371
Strike50 <lhoulgattebustamante@gmail.com>
2018-12-11 16:08:00 +0100

    remember me login logout

--- src/main/java/com/gfi/springit/security/SecurityConfiguration.java
+++ src/main/java/com/gfi/springit/security/SecurityConfiguration.java
@@ -23,16 +23,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .requestMatchers(EndpointRequest.to("info")).permitAll()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
-                .antMatchers("/actuator/").hasRole("ADMIN")
-                .antMatchers("/").permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
+                .antMatchers("/actuator/").hasRole("ACTUATOR")
                 .antMatchers("/link/submit").hasRole("USER")
+                .antMatchers("link/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/vote/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
             .and()
-            .formLogin()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .usernameParameter("email")
             .and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
+                .logout()
+            .and()
+                .rememberMe();
     }
 
     @Override
