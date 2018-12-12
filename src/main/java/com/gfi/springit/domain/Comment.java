24a4954d4c0d5cb0947112c9be224bcdddb329e7
Strike50 <lhoulgattebustamante@gmail.com>
2018-12-12 11:15:02 +0100

    The ability to add a new comment

--- src/main/java/com/gfi/springit/domain/Comment.java
+++ src/main/java/com/gfi/springit/domain/Comment.java
@@ -1,22 +1,27 @@
 package com.gfi.springit.domain;
+import com.gfi.springit.service.BeanUtil;
 import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @RequiredArgsConstructor
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 public class Comment extends Auditable {
 
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
     private String body;
 
@@ -24,4 +29,12 @@ public class Comment extends Auditable {
     @NonNull
     private Link link;
 
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
+    }
 }
